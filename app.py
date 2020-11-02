54c06b63e0cf4184abd2a252cbf774903ea3458d
Jack Wu <masterjack1997@gmail.com>
2020-11-02 15:25:03 -0500

    get actors working

--- app.py
+++ app.py
@@ -2,15 +2,59 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import setup_db, Actor, Movie
+from auth import AuthError, requires_auth
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
+  setup_db(app)
   CORS(app)
-
   return app
 
 app = create_app()
+
+
+@app.route('/')
+def home(): 
+  return "Hello World"
+
+@app.route('/actors', methods=['GET'])
+def get_actors(): 
+  actors = Actor.query.all()
+  return jsonify({
+    'success': True, 
+    'actors': actors
+  }), 200
+
+
+#Dcoument how to make post request: http://0.0.0.0/actors?name=<string:name>&age=<int:age>&gender=<string:gender>
+@app.route('/actors', methods=['POST'])
+@requires_auth('post:actors')
+def add_actor(payload):
+  name = request.args.get('name')
+  age = request.args.get('age')
+  gender = request.args.get('gender')
+
+  try: 
+    actor = Actor(
+      name = name, 
+      age = age, 
+      gender = gender 
+    )
+    actor.insert()
+  except: 
+    abort(400)
+
+  return jsonify({
+    'success': True, 
+    'actor': actor
+  }), 200
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
--- models.py
+++ models.py
@@ -1,1 +1,44 @@
 
+import os
+from flask_sqlalchemy import SQLAlchemy 
+from sqlalchemy import Column, String, Integer, Date, create_engine 
+
+
+
+database_name = "casting_agency"
+database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+def setup_db(app): 
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.drop_all()
+    db.create_all()
+
+
+class Movie(db.Model): 
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=-True)
+    title = Column(String)
+    release = Column(Date)
+
+
+class Actor(db.Model): 
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+
+    def insert(self): 
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self): 
+        db.session.delete(self)
+        db.session.commit()
